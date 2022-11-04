2e609ac84b66399280a8e9e9184bd7c413966ce2
Max Hjärpe <hjarpemax@gmail.com>
2022-11-04 09:48:07 +0100

    init

---
+++ ContactSurfaceController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+using Umbraco.Cms.Infrastructure.Scoping;
+
+namespace Ummmmbraco
+{
+    public class ContactFormDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+    public class ContactSurfaceController : SurfaceController
+    {
+        private IScopeProvider _scopeProvider; 
+        public ContactSurfaceController(
+            IScopeProvider scopeProvider,
+            IUmbracoContextAccessor umbracoContextAccessor,
+            IUmbracoDatabaseFactory databaseFactory,
+            ServiceContext services,
+            AppCaches appCaches,
+            IProfilingLogger profilingLogger,
+            IPublishedUrlProvider publishedUrlProvider)
+            : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+            _scopeProvider = scopeProvider;
+        }
+
+        public IActionResult ContactSubmit(ContactFormDto form)
+        {
+            if (form.Name == null || form.Email == null || form.Message == null)
+                return BadRequest();
+
+            using (var scope = _scopeProvider.CreateScope())
+            {
+                var data = new AddCommentsTable.Contactschema();
+                data.Name = form.Name;
+                data.Email = form.Email;
+                data.Message = form.Message;
+
+                scope.Database.Insert(data);
+                scope.Complete();
+            }
+                return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ DataManager.cs
@@ -0,0 +1,34 @@
+﻿namespace Ummmmbraco
+{
+    //http://gaddr.co/ios/stations
+    public class Root
+    {
+        public List<Station> Stations { get; set; } 
+    }
+
+    public class Station
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+        public string description { get; set; }
+        public Coordinates coordinates { get; set; }
+        public double rating { get; set; }
+        public bool paid { get; set; }
+    }
+    public class Coordinates
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+    }
+
+    public class DataManager
+    {
+        public static async Task<Root> GetStationsAsync()
+        {
+            HttpClient client = new HttpClient();
+            Root? data = await client.GetFromJsonAsync<Root>("http://gaddr.co/ios/stations");
+            return data;
+        }
+    }
+}
